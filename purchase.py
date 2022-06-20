10c62f95c7628a77934ceb42401a4b783361c00c
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2022-06-20 10:29:00 +0200

    Delete shipment_party field.
    Task #045266

--- purchase.py
+++ purchase.py
@@ -8,45 +8,26 @@ from trytond.pyson import If, Eval, Bool
 class Purchase(metaclass=PoolMeta):
     __name__ = 'purchase.purchase'
 
-    shipment_party = fields.Many2One('party.party', 'Shipment Party',
-        states={
-            'readonly': (Eval('state') != 'draft'),
-            },
-        context={
-            'company': Eval('company', -1),
-            },
-        search_context={
-            'related_party': Eval('party'),
-            },
-        depends=['state', 'party', 'company'])
     shipment_address = fields.Many2One('party.address', 'Shipment Address',
         domain=[
-            ('party', '=', If(Bool(Eval('shipment_party')),
-                    Eval('shipment_party'), Eval('party'))),
+            ('party', '=', If(Bool(Eval('party')),
+                    Eval('party'), Eval('party'))),
             ],
         states={
             'readonly': Eval('state') != 'draft',
             'required': ~Eval('state').in_(
                 ['draft', 'quotation', 'cancelled']),
             },
-        depends=['party', 'shipment_party', 'state'])
+        depends=['party', 'state'])
 
-    @fields.depends('party', 'shipment_party', 'payment_term', 'lines')
+    @fields.depends('party', 'payment_term', 'lines', 'shipment_address')
     def on_change_party(self):
         super().on_change_party()
-        if not self.shipment_party:
+        if not self.party:
             self.shipment_address = None
         if self.party:
             if not self.shipment_address:
                 self.shipment_address = self.party.address_get(type='delivery')
-
-    @fields.depends('party', 'shipment_party')
-    def on_change_shipment_party(self):
-        if self.shipment_party:
-            self.shipment_address = self.shipment_party.address_get(
-                type='delivery')
-        elif self.party:
-            self.shipment_address = self.party.address_get(type='delivery')
 
 
 class PurchaseLine(metaclass=PoolMeta):
